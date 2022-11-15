b629330913247f8d477364c5af18e5de0ab31c66
ESPORTS-133\cre <t-shirai@oca.ac.jp>
2022-11-15 18:28:05 +0900

    完了履歴画面まで =>未完了に戻すボタンから

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\History;
 use App\Models\Task;
+use App\Models\History;
+
 use Illuminate\Support\Facades\Validator;
 
 class HistoryController extends Controller
@@ -17,9 +17,9 @@ class HistoryController extends Controller
      */
     public function index()
     {
-    $tasks = Task::where('status', true)->get();
-    
-    return view('history.index', compact('history'));
+        $history = Task::where('status', true)->get();
+  
+        return view('history.index', compact('history'));
     }
 
     /**
@@ -40,7 +40,25 @@ class HistoryController extends Controller
      */
     public function store(Request $request)
     {
-        return redirect('/tasks');
+ 
+        $rules = [
+            //
+        ];
+
+         
+  
+        //モデルをインスタンス化
+        $history = new History;
+        
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $history->name = $request->input('history_name');
+        
+        //データベースに保存
+        $history->save();
+        
+        //リダイレクト
+        return redirect('/History');
+ 
     }
 
     /**
@@ -74,30 +92,24 @@ class HistoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if ($request->status === null) {
-            $rules = [
-              'task_name' => 'required|max:100',
-            ];
-        
-        
-            //データベースに保存
-            $task->save();
-          } else {
-            //「完了」ボタンを押したとき
-        
-            //該当のタスクを検索
-            $task = Task::find($id);
-        
-            //モデル->カラム名 = 値 で、データを割り当てる
-            $task->status = fale; //true:完了、false:未完了
-        
-            //データベースに保存
-            $task->save();
-          }
-        
-        
-          //リダイレクト
-          return redirect('/tasks');
+    //「編集する」ボタンをおしたとき
+    if ($request->status === 0) {
+
+        //「完了」ボタンを押したとき
+    
+        //該当のタスクを検索
+        $history = Task::find($id);
+    
+        //モデル->カラム名 = 値 で、データを割り当てる
+        $history->status = true; //true:完了、false:未完了
+    
+        //データベースに保存
+        $history->save();
+      }
+    
+    
+      //リダイレクト
+      return redirect('/history');
     }
 
     /**
--- database/migrations/2022_11_08_170406_create_history_table.php
+++ database/migrations/2022_11_08_170406_create_history_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('history', function (Blueprint $table) {
+        Schema::create('History', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
             $table->boolean('status')->default(false);
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('history');
+        Schema::dropIfExists('History');
     }
 };
